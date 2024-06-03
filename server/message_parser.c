1acf56a2121b087b1912f1c284bbeb191ab8c7cb
asdf <asdf>
2024-06-03 14:07:03 +0000

    fix

--- server/message_parser.c
+++ server/message_parser.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+void clear_buffer(char buffer[CONTENT_SIZE]) {
+    for(int i = 0; i < CONTENT_SIZE; i++) {
+        buffer[i] = 0;
+    }
+}
+
 server_message parse_to_server_msg(char *msg) {
     server_message result;
     result.type = msg[0];
@@ -10,11 +16,13 @@ server_message parse_to_server_msg(char *msg) {
 }
 
 void parse_server_msg(server_message message, char buffer[CONTENT_SIZE]) {
+    clear_buffer(buffer);
     buffer[0] = message.type;
     strcpy(buffer+1, message.content);
 }
 
 void parse_client_msg(client_message message, char buffer[CONTENT_SIZE]) {
+    clear_buffer(buffer);
     buffer[0] = message.type;
     strcpy(buffer+1, message.content);
 }
