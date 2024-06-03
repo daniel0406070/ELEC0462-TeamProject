1492fd1a6c75bea8c6ffc24fe0969fc5a509a93d
hongs0312 <hongseongho300@gmail.com>
2024-06-03 20:22:28 +0900

    typectr.h 수정

--- client/totalset.h
+++ client/totalset.h
@@ -1,32 +1,32 @@
 #define _totalset_h
 
 // -- Default Game Settings --
-#define MAX_PLAYER 4
-#define ROUND 5
-#define CONTENT_SIZE 128
+#define MAX_PLAYER 	4
+#define ROUND 		5
+#define CONTENT_SIZE 	128
 
-#define BUF_SIZE 100
-#define NAME_SIZE 20
+#define BUF_SIZE 	100
+#define NAME_SIZE 	20
 
-#define MAXLEN_STR 200
+#define MAXLEN_STR 	200
 
 // -- Client Settings --
-#define JOIN 1
-#define START 2
-#define TYPING 3
-
-#define START_GAME	"start game"
-#define END_GAME	"end game"
+#define JOIN 	1
+#define START 	2
+#define TYPING 	3
 
 // -- Server Settings --
 #define BROADCAST 1
-
+#define WRONG	  2
+#define CORRECT	  3
+#define POINT	  4
+#define GAMEEND	  5
 // -- Game State
-#define READY 0
+#define READY 	0
 #define PLAYING 1
 
 // -- Display Settings --
-#define LEFT_EDGE 5
-#define RIGHT_EDGE 35
-#define TOP_ROW 5
-#define BOT_ROW 55
+#define LEFT_EDGE 	5
+#define RIGHT_EDGE 	35
+#define TOP_ROW 	5
+#define BOT_ROW 	55
